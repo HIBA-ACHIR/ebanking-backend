116d563f7819a3f1d2eef6ad66cfa8c9aeee894d
HIBA-ACHIR <hibatallahachir@.com>
2024-05-11 13:57:00 +0100

    update

--- src/main/java/org/sid/ebankingbackend/repositories/AccountOperationRepository.java
+++ src/main/java/org/sid/ebankingbackend/repositories/AccountOperationRepository.java
@@ -2,7 +2,14 @@ package org.sid.ebankingbackend.repositories;
 
 import org.sid.ebankingbackend.entities.AccountOperation;
 import org.sid.ebankingbackend.entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation> findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
+
 }
